095d58b21cc8027fbc7bbfca40d36afa05219ae8
ysfmetu <ysf_metu@hotmail.com>
2022-07-18 18:00:34 +0300

    #form api yi reactive forma çevirmek 1_her property için form component içerisinde yeni bir form control oluşturuyoruz. 2_form control validators ile validation yapıyoruz..

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -4,7 +4,7 @@ import {ModelRepo} from "../model/repo.service";
 import {MODES, SharedState, StateUpdate} from "./sharedState.service";
 import {MessageService} from "../messages/message.service";
 import {Message} from "../messages/message.model";
-import {FormControl, NgForm} from "@angular/forms";
+import {FormControl, NgForm, Validators} from "@angular/forms";
 
 @Component({
   selector: "app-form",
@@ -14,7 +14,12 @@ import {FormControl, NgForm} from "@angular/forms";
 export class FormComponent{
   product: Product = new Product();
   editing: boolean = false;
-  nameField:FormControl=new FormControl("ysf")
+  nameField:FormControl=new FormControl("ysf",
+      {validators:[Validators.maxLength(14),
+                    Validators.minLength(3),
+                    Validators.pattern("^[A-Za-z ]+$")]})
+  categoryField:FormControl=new FormControl("",
+      {validators:Validators.minLength(4)})
 
   constructor(private model: ModelRepo, private state: SharedState,
               private messageService: MessageService) {
@@ -29,12 +34,34 @@ export class FormComponent{
       this.messageService.reportMessage(
           new Message(`<<${this.product.name}>> isimli kayıt düzenleniyor`));
       this.nameField.setValue(this.product.name)
+      this.categoryField.setValue(this.product.category)
     } else {
     this.product = new Product();
     this.messageService.reportMessage(new Message("Yeni Kayıt Oluşturuluyor"));
-    this.nameField.setValue("ff")
+    this.nameField.setValue("")
+    this.categoryField.setValue("")
   }
 }
+  ngOnInit(){
+    this.nameField.statusChanges.subscribe(newStatus => {
+      if (newStatus == "INVALID" && this.nameField.errors != null) {
+        let errs = Object.keys(this.nameField.errors).join(", ");
+        this.messageService.reportMessage(new Message(`INVALID: ${errs}`))
+      } else {
+        this.messageService.reportMessage(new Message(newStatus));
+      }
+      this.categoryField.statusChanges.subscribe(newStatus=>{
+        if(newStatus=="INVALID"){
+          this.categoryField.disable();
+        }else
+          this.categoryField.enable();
+      })
+
+//valueChanges ile her değişimi ekrana bastık
+   /* this.nameField.valueChanges.subscribe(newValue=>{
+      this.messageService.reportMessage(new Message(newValue||"isim alanı boş !!"))
+  */  })
+  }
 submitForm(form: NgForm) {
   if (form.valid) {
     this.model.saveProduct(this.product);
